7eb4835e52f88a6951c83043d0e49b5a09817f2e
Julia Lu Dac <julialudac@yahoo.fr>
2020-07-19 15:31:52 +0200

    Display questions and asks for input: Part 1 planning

---
+++ main.py
@@ -0,0 +1,10 @@
+
+print('Welcome to the "Double your productivity by 5 P.M. tomorrow system!"')
+
+print("----------- Phase 1 - Planning -------------")
+
+dum = input("1/ What's your big goal (Month/Quarter/Year)? ")
+
+print("2/ Amplify your motivation for the Big Goal. Create a burning Desire.")
+dum = input("2/ a. Visualize (OK/KO): ")
+
