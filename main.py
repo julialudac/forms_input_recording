eb4f74654237d51cb055e6a71e5bbe84b05c9395
Julia Lu Dac <julialudac@yahoo.fr>
2020-07-19 15:31:52 +0200

    Display questions and asks for input: Part 1 planning

---
+++ main.py
@@ -0,0 +1,31 @@
+
+print('Welcome to the "Double your productivity by 5 P.M. tomorrow system!"')
+
+print("----------- Phase 1 - Planning -------------")
+
+dum = input("1/ What's your big goal (Month/Quarter/Year)? ")
+
+print("2/ Amplify your motivation for the Big Goal. Create a burning Desire.")
+dum = input("2/ a. Visualize (OK/KO): ")
+dum = input("2/ c. Write reasons for wanting that goal: ")
+
+print("3/ What 3 things max do you want to accomplish by 5 PM tomorrow? (Mini Goals)")
+print("A goal as a precise type: either a process goal, an outcome goal or a time investment goal. So write each one of the goal by specifying its type.")
+dum = input("3/ a. Goal 1: ")
+dum = input("3/ b. Goal 2: ")
+dum = input("3/ c. Goal 3: ")
+
+print("4/ End time + fun tomorrow?")
+dum = input("4/ a. By what time will you finish your work tomorrow (hint: 5 PM)? ")
+dum = input("5/ b. What would be the most FUN thing you could do after you finish work? Schedule it in calendar: ")
+
+print("5/ Build accountability/Commitment")
+dum = input("5/ a. Name an accountability partner: ")
+dum = input("5/ b. Fine/punishment if the mini goals are not accomplished by EOD: ")
+
+print("6/ Block time on your calendar for tomorrow")
+dum = input("6/ a. Morning routine: 30 to 60 minutes :")
+dum = input("6/ b. Fine/punishment if the mini goals are not accomplished by EOD: ")
+
+# etc...
+
