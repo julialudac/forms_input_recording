74c83e8ede29bffe824656ee84a7f915ddb07392
julialudac <julialudac@yahoo.fr>
2020-09-06 17:13:38 +0200

    Replace python by python3 in the runTests or do something to avoid the python: command not found #19

--- runTests.sh
+++ runTests.sh
@@ -10,10 +10,15 @@ fi
 
 # Run component tests
 echo "Running component test..."
+python_cmd=python
+python_exists=$(command -v python)
+if [ "$python_exists" == "" ]; then
+    python_cmd=python3
+fi
+
 # TODO: Now there is only one component test...
-#cd component_tests
 expected=$(cat component_tests/multiple_forms_out.txt | tr '\n' ' ') 
-actual=$(python main.py < component_tests/multiple_forms_in.txt | tr '\n' ' ')
+actual=$($python_cmd main.py < component_tests/multiple_forms_in.txt | tr '\n' ' ')
 if [ ! "$expected" == "$actual" ]; then
     echo "Component test failed!"
     echo "Expected value: $expected"
